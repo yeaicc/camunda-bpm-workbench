c4144b9f646fa566cb47c545a9b8ce99f1d56d7d
Nico Rehwaldt <git_nikku@nixis.de>
2014-12-17 15:11:42 +0100

    feat(client/diagram): integrate undo/redo

--- webapp/app/lib/diagram/directive/dgrCanvas.js
+++ webapp/app/lib/diagram/directive/dgrCanvas.js
@@ -28,6 +28,47 @@ module.exports = function() {
     template: directiveTemplate,
     link: function(scope, element) {
 
+      // listen to undo / redo action on an element
+      // triggered by (CTRL|META)+(Y|Z)
+
+      function listenUndoRedo(e) {
+        if (e.ctrlKey || e.metaKey) {
+
+          if (e.which === 89 || e.which === 90) {
+            e.preventDefault();
+            e.stopPropagation();
+          }
+
+          // Y = 89
+          if (e.which === 89) {
+            try {
+              scope.diagramManager.renderer.get('commandStack').redo();
+            } catch (e) {
+              // jup
+            }
+          }
+
+          // Z = 90
+          if (e.which === 90) {
+            try {
+              scope.diagramManager.renderer.get('commandStack').undo();
+            } catch (e) {
+              // jup
+            }
+          }
+        }
+      }
+
+      // capture undo / redo if mouse focus is active only
+
+      element.on('mouseenter', function(e) {
+        document.addEventListener('keydown', listenUndoRedo);
+      });
+
+      element.on('mouseleave', function(e) {
+        document.removeEventListener('keydown', listenUndoRedo);
+      });
+
       scope.$watch('perspective', function(newValue) {
 
         var diagramManager = scope.diagramManager;
