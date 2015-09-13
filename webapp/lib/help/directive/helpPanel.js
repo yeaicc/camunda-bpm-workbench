9b4e202bc0a59d0bcaf64daca3ee75f5d3005550
Thorben Lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2015-09-13 16:46:27 +0200

    access solr instance via SSL

--- webapp/lib/help/directive/helpPanel.js
+++ webapp/lib/help/directive/helpPanel.js
@@ -28,7 +28,7 @@ var HelpPanelController = [
         $scope.executeSearch = function() {
             var q = "q=" + encodeURIComponent($scope.searchQuery.replace(' ', '+'));
             var fragSize = 300; // length of snippet
-            var getUrl = "http://ec2-52-19-129-229.eu-west-1.compute.amazonaws.com:8080/solr/" + "hackdays" + "/select?" + q + "&fl=title,link,type,id&wt=json&hl=true&hl.fl=text&hl.fragsize=" + fragSize;
+            var getUrl = "https://ec2-52-19-129-229.eu-west-1.compute.amazonaws.com:8080/solr/" + "hackdays" + "/select?" + q + "&fl=title,link,type,id&wt=json&hl=true&hl.fl=text&hl.fragsize=" + fragSize;
             $scope.lastSearchQuery = $scope.searchQuery;
 
             console.log(getUrl);
